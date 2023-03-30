1beadeff5d3f7bf9b98c8b42f8f2d60c62490d8d
thecil <thecilsurf@gmail.com>
2023-03-30 15:23:32 -0400

    vercel

--- client/src/server.js
+++ client/src/server.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const server = axios.create({
-  baseURL: import.meta.env.VITE_SERVER_BASE_URL,
+  baseURL: "https://ecdsa-server-orpin.vercel.app",
 });
 
 export default server;
